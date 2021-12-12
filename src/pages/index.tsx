4683953763e4cb0137f338f4f429674165788916
Gease-RS <geasebrasil@gmail.com>
2021-12-11 21:28:22 -0300

    Chakra Form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,57 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
-      <h1>Dashboard</h1>
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
     </Flex>
   );
 }

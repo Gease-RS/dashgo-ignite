c1ed4bd432cc5e1ca5daaca4f0645660867926f9
Gease-RS <geasebrasil@gmail.com>
2021-12-15 21:44:16 -0300

    Trocando de página

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,15 +16,18 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
-import Pagination from "../../components/Pagination";
+import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
+
+  console.log(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -81,7 +84,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]} color="gray.300" width="8">
@@ -113,7 +116,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
